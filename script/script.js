c38c8b28a5ed4cc881620b7d87fa25f8c3c4d7b9
Christian de Abreu Borges <christianborges27@hotmail.com>
2025-03-17 20:27:43 -0300

    adicao de lista de pendentes

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 const btnAdd = document.getElementById('btnAdd');
+const btnAddFinal = document.getElementById('btnAddFinal')
 const listaComprado = document.getElementById('listaComprado');
+const listaPendente = document.getElementById('listaPendente');
 
 const btnAddClick = () => {
     const nomeProduto = document.getElementById('nomeProduto').value;
@@ -9,7 +11,7 @@ const btnAddClick = () => {
     if (nomeProduto && qtdProduto && dscProduto) {
         const li = document.createElement('li');
         li.innerText = `Nome: ${nomeProduto}  \n Quantidade: ${qtdProduto} \n Descrição: ${dscProduto}`;
-        listaComprado.appendChild(li);
+        listaPendente.appendChild(li);
 
         document.getElementById('nomeProduto').value = '';
         document.getElementById('qtdProduto').value = '';
@@ -17,4 +19,19 @@ const btnAddClick = () => {
     }
 };
 
+const btnAddFinalClick = () => {
+    const itensPendentes = listaPendente.getElementsByTagName('li');
+
+    for (const elementos of itensPendentes) {
+        const li1 = document.createElement('li');
+        li1.innerText = elementos.innerText;
+        listaComprado.appendChild(li1);
+    }
+
+    while (listaPendente.firstChild) {
+        listaPendente.removeChild(listaPendente.firstChild);
+    }
+};
+
 btnAdd.addEventListener('click', btnAddClick);
+btnAddFinal.addEventListener('click', btnAddFinalClick)
